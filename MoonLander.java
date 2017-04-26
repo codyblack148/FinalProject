69baf5b4ea72745d0e0cca32f5006714a4196b0b
Cody Black <cody.black148@gmail.com>
2017-04-26 01:06:09 -0500

    Update view and mainlander, added two gifs
    Massive changes to both view and moonlander. Game is in better state than it was. In order to be working at its core, we need the methods move, thrust, and changeVelocity in the MoonLander class to be reconfigured to work properly. Just about everything else works, other than working out kinks for end of game. Just pull everything from all files to continue work, the only thing I did not touch was the Leaderboard class.

--- MoonLander.java
+++ MoonLander.java
@@ -1,21 +1,32 @@
 import java.lang.Math;
+
+import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.input.KeyCode;
 
+/**
+ * @author codyblack
+ *
+ */
 public class MoonLander{
 
+	/**
+	 * Moonlander class holds code to create the MoonLander, all of its attributes,
+	 * and update its position to the UI.
+	 */
 	private float xlocation = 350, ylocation = 150;
 	private float vx,vy,ax,ay, height;
-	private boolean isLanded;
-	private final int MIN_THRUST = 0, MAX_THRUST = 100, MIN_FUEL = 1,DELTA=1;
-	private double directionAngle=0,  fuelLevel = 2000, thrust = 0,gravity = 9.8;
-	private int checkThrust = 0;
+	private final int MIN_THRUST = 1, MAX_THRUST = 100, MIN_FUEL = 1,DELTA=1;
+	private double directionAngle=0,  fuelLevel = 2000, thrust = 0,gravity = 1.6;
 	ImageView imageView;
     Image image;
 	Pane layer;
+	float timeAcc;
+	float timeVel;
+	public TimeTracker accTimer = new TimeTracker(1,1);
 	
 	
 	public MoonLander(Image image, Pane layer){
@@ -24,7 +35,8 @@ public class MoonLander{
 		ax=0;
 		ay=0;
 		height = 650;
-		isLanded=false;
+		xlocation = 350;
+		ylocation = 150;
 		this.image=image;
 		this.layer=layer;
 		this.imageView = new ImageView(image);
@@ -61,13 +73,16 @@ public class MoonLander{
 	
 	}
 	public void reset(){
-		fuelLevel=2000;
 		vx=0;
 		vy=0;
 		ax=0;
 		ay=0;
-		thrust=0;
-		isLanded=false;
+		height = 650;
+		xlocation = 350;
+		ylocation = 150;
+		this.imageView.relocate(350, 150);
+		
+		
 		
 	}
 	public void rotateLeft(){
@@ -79,8 +94,23 @@ public class MoonLander{
 		imageView.setRotate(directionAngle);
 	}
 	public void move(){
-		xlocation = (xlocation + vx*Timer.check()+1/2*ax*Timer.check()*Timer.check())/5;
-		ylocation = -(ylocation + vy*Timer.check()+1/2*ay*Timer.check()*Timer.check())/5;
+		//xlocation;
+		
+		timeAcc = accTimer.getTime()/1000;
+		ylocation += vy*timeAcc;
+		xlocation += vx*timeAcc;
+		if(xlocation<1){
+			xlocation=0;
+		}
+	    if(xlocation>800){
+			xlocation=799;
+		}
+		if(ylocation<1){
+			ylocation = 1;
+		}
+				
+		/*	xlocation = (xlocation + vx*time+1/2*ax*time*time)/5;
+		ylocation = -(ylocation + vy*time+1/2*ay*time*time)/5;
 		if(ylocation>749){
 			ay=0;
 			vy=0;
@@ -91,15 +121,15 @@ public class MoonLander{
 		}
 		else if(xlocation>749){
 			xlocation=750;
-		}
+		}*/
 	}
 	public void setThrust(double thrust){
 		this.thrust=thrust;
 	}
 	public void thrust(double thrust){
-		if(fuelLevel < MIN_FUEL){
+		if(fuelLevel < MIN_FUEL || thrust < MIN_THRUST){
 			this.ax=0;
-			this.ay=(float) gravity;//lander is out of fuel, falls to Tatooine.
+			this.ay=(float) -gravity;//lander is out of fuel, falls to Tatooine.
 		} else {
 			this.ax = (float) (thrust/100*45040/(15000+fuelLevel)*Math.sin(directionAngle));//lander is at max thrust, nothing happens.
 			this.ay = (float) (thrust/100*45040/(15000+fuelLevel)*-1*Math.cos(directionAngle)+gravity);
@@ -109,15 +139,13 @@ public class MoonLander{
 	public double getThrust(){
 		return thrust;
 	}
-	public boolean checkThrust(){
-		return thrust > 0;
-	}
 	public float getVelocity(){
 		return (float) Math.sqrt((vx*vx) + (vy*vy));
 	}
 	public void changeVelocity(){
-		vx = vx + ax*Timer.check();
-		vy = -vy - ay*Timer.check();
+		timeVel = accTimer.getTime()/1000;
+		vx = vx + ax*(timeVel);
+		vy = -vy - ay*(timeVel);
 		
 	}
 	public double getFuel(){
@@ -134,31 +162,20 @@ public class MoonLander{
 		}
 	}
 	public float getHeight(){
-		return ylocation;
+		return -(ylocation - 650);
 	}
 	public void processInput(KeyCode k, Pane layer){
 		switch(k){
 		case UP: System.out.println("Increase thrust");
-		Timer.reset();
+		accTimer.reset();
 		thrust +=5;
-		checkThrust++;
-		if(checkThrust==1){
-			layer.getChildren().remove(imageView);
-			this.image = new Image( getClass().getResource("MoonLanderAcc.png").toExternalForm());
-			this.imageView = new ImageView(image);
-			layer.getChildren().add(imageView);
-		}
 		break;
 		case DOWN: System.out.println("Decrease thrust.");
-		Timer.reset();
-		thrust -=5;
-		checkThrust--;
-		if(checkThrust==0){
-			layer.getChildren().remove(imageView);
-			this.image = new Image( getClass().getResource("MoonLander.png").toExternalForm());
-			this.imageView = new ImageView(image);
-			layer.getChildren().add(imageView);
-		}
+		accTimer.reset();
+		if(! (thrust < MIN_THRUST)){
+			thrust -=5;
+			}
+		
 		;break;
 		
 		case RIGHT: System.out.println("Rocket rotate right.");
@@ -171,26 +188,40 @@ public class MoonLander{
 		;break;
 		case SPACE: System.out.println("Maximum thrust.");
 		thrust = MAX_THRUST;
-		Timer.reset();
+		accTimer.reset();
 		break;
 		default: System.out.println("Nothing")
 		
 		;break;
 		}
 	}
+	
+
 	public float getX(){
 		return xlocation;
 	}
 	public float getY(){
 		return ylocation;
 	}
+
+	public float getVx(){
+		return vx;
+	}
+	public float getVy(){
+		return vy;
+	}
+	public float getAx(){
+		return ax;
+	}
+	public float getAy(){
+		return ay;
+	}
 	public void updateLocation(float x, float y, double r){
 		if(fuelLevel<MIN_FUEL){
 			thrust=0;
 		}
-		thrust(thrust);
+		
 		changeVelocity();
-		move();
 		imageView.relocate(x, y);
 		imageView.setRotate(r);
 	}
--- View.java
+++ View.java
@@ -1,39 +1,45 @@
 import javafx.scene.paint.Color;
 import javafx.application.Application;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
-import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.animation.*;
+import java.io.File;
 import java.util.Random;
+
 public class View extends Application {
-	public Stage primaryStage;
-	Image rocketOff = new Image( getClass().getResource("MoonLander.png").toExternalForm());
+	public Stage stage;
 	Image rocketOn = new Image( getClass().getResource("MoonLanderAcc.png").toExternalForm());
 	Image target = new Image( getClass().getResource("targettemp.png").toExternalForm());
 	ImageView image = new ImageView(target);
 	Random rand = new Random();
-
+	int targetLocation;
+	
+	
 	public void start(Stage primaryStage) {
 		
+		/* 
+		 * Main game scene and initialization.
+		 */
+		if(stage==null){
+			stage = primaryStage;
+		}
 		
-		Stage stage = primaryStage;
 		BorderPane root = new BorderPane();
 		Scene scene = new Scene(root,800,800);
 		scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
@@ -41,66 +47,131 @@ public class View extends Application {
 		BorderPane topGUI = new BorderPane();
 		root.setTop(topGUI);
 		topGUI.setId("title");
-		MoonLander lander = new MoonLander(rocketOff,root);
-		
-		int targetLocation = generateTarget(root);
-		
+		MoonLander lander = new MoonLander(rocketOn,root);
+		targetLocation = generateTarget(root);
 		//Top left Vbox of the GUI that displays the stats for the rocket.
-		VBox leftStats = new VBox(12);
-		leftStats.setPrefWidth(200);
-		leftStats.setPrefHeight(150);
-		leftStats.setStyle("-fx-background-color: black;");
-		Label l1 = new Label("Fuel: ");
-		Label l2 = new Label("Velocity: ");
-		Label l3 = new Label("Thrust %: ");
-		Label l4 = new Label("Orientation: ");
-		l1.setTextFill(Color.WHITE);
-		l2.setTextFill(Color.WHITE);
-		l3.setTextFill(Color.WHITE);
-		l4.setTextFill(Color.WHITE);
-		leftStats.getChildren().addAll(l1,l2,l3,l4);
-		topGUI.setLeft(leftStats);
+				VBox leftStats = new VBox(12);
+				leftStats.setPrefWidth(200);
+				leftStats.setPrefHeight(150);
+				leftStats.setStyle("-fx-background-color: black;");
+				Label l1 = new Label("Fuel: ");
+				Label l2 = new Label("Velocity: ");
+				Label l3 = new Label("Thrust %: ");
+				Label l4 = new Label("Orientation: ");
+				l1.setTextFill(Color.WHITE);
+				l2.setTextFill(Color.WHITE);
+				l3.setTextFill(Color.WHITE);
+				l4.setTextFill(Color.WHITE);
+				leftStats.getChildren().addAll(l1,l2,l3,l4);
+				topGUI.setLeft(leftStats);
+				//Top right Vbox of the GUI, shows height in meters and elapsed time.
+				VBox rightStats = new VBox(12);
+				rightStats.setPrefWidth(200);
+				rightStats.setPrefHeight(150);
+				rightStats.setStyle("-fx-background-color: black");
+				Label l5 = new Label("Time Elapsed: ");
+				Label l6 = new Label("Height: ");
+				l5.setTextFill(Color.WHITE);
+				l6.setTextFill(Color.WHITE);
+				rightStats.getChildren().addAll(l5,l6);
+				topGUI.setRight(rightStats);
+				
+			scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+				public void handle(KeyEvent e){
+					lander.processInput(e.getCode(),root);
+				
+				}
+				
+			});
+			//TimeTracker timer = new TimeTracker();
+				
+				stage.setScene(scene);
+				stage.show();
 		
-		//Top right Vbox of the GUI, shows height in meters and elapsed time.
-		VBox rightStats = new VBox(12);
-		rightStats.setPrefWidth(200);
-		rightStats.setPrefHeight(150);
-		rightStats.setStyle("-fx-background-color: black");
-		Label l5 = new Label("Time Elapsed: ");
-		Label l6 = new Label("Height: ");
-		l5.setTextFill(Color.WHITE);
-		l6.setTextFill(Color.WHITE);
-		rightStats.getChildren().addAll(l5,l6);
-		topGUI.setRight(rightStats);
+				/* 
+				 * Win, play again scene setup. Initialization located in AnimationTimer.
+				 */
+				Pane winPlayAgain = new Pane();
+				Scene scene2 = new Scene(winPlayAgain,400,363);
+				scene2.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+				winPlayAgain.setId("Vader");
+				Button btn1 = new Button("Play Again?");
+				Button btn2 = new Button("Quit");
+				btn1.setTranslateX(280);
+				btn1.setTranslateY(325);
+				btn2.setTranslateX(25);
+				btn2.setTranslateY(325); 
+				winPlayAgain.getChildren().addAll(btn1,btn2);
+				
 		
-	scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
-		public void handle(KeyEvent e){
-			lander.processInput(e.getCode(),root);
-		}
 		
-	});
 		
-		stage.setScene(scene);
-		stage.show();
 		
+		
+		/* 
+		 * Game animation loop initialization.
+		 */
 		AnimationTimer gameLoop = new AnimationTimer() {
 
             @Override
             public void handle(long now) {
+            	
             	l1.setText("Fuel: "+ lander.getFuel());
             	l2.setText("Velocity: "+ lander.getVelocity());
             	l3.setText("Thrust %: "+ lander.getThrust());
             	l4.setText("Direction Angle: "+ lander.getDirectionAngle());
+            	//l5.setText("Time Elapsed: "+timer.getTime());
             	l6.setText("Height: " + lander.getHeight());
+            	
             	lander.updateLocation(lander.getX(), lander.getY(), lander.getDirectionAngle());
+            	lander.move();
+            	lander.thrust(lander.getThrust());
+            	
+            	if(lander.getY()>650){ //Hits the Ground
+            		
+            		if(lander.getDirectionAngle() < 20 || lander.getDirectionAngle() > 345){//Check to see if it Crashed (check Velocity and angles)
+            			System.out.println(targetLocation);
+            			if((targetLocation - 75) < lander.getX() && lander.getX() < (targetLocation+75)){ //Is it on the target
+            				
+                    			System.out.println("Hit the Target!");
+                    			primaryStage.setScene(scene2);
+                    			primaryStage.show();
+                    		}
+            			System.out.println("You landed, ya piece of shit.");
+            		}
+            		else{
+            			System.out.println("You Crashed, ya piece of shit.");
+            		}
+            		
+            		stop();
+            	}
+            	
+
             }
 
         };
-        gameLoop.start();
+       gameLoop.start();
+       btn1.setOnAction((event) -> {
+    	    System.out.println("Play again.");
+    	    lander.reset();
+    	    removeTarget(root,image);
+    	    targetLocation = generateTarget(root);
+    	    stage.setScene(scene);
+       	    stage.show();
+       	    gameLoop.start();
+    	});
+       btn2.setOnAction((event) -> {
+   	    System.out.println("Quit");
+
+   	});
+       
+       
+       
 	}
 	public static void main(String[] args){
 		launch(args); //move?
 	}
+
 	private int generateTarget(Pane layer){
 		int x = rand.nextInt(601) + 100;
 		image.setFitHeight(100);
@@ -109,5 +180,10 @@ public class View extends Application {
 		layer.getChildren().add(image);
 		return x;
 	}
+	private void removeTarget(Pane layer, Node target){
+		layer.getChildren().remove(target);
+	}
+
+	
 }
 
