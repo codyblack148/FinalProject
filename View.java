cbc37b5f6048ea449724a9e075ac704bd35a6319
Cody Black <cody.black148@gmail.com>
2017-04-26 02:38:57 -0500

    Massive overhaul of view
    Absolutely massive overhaul, again, to the view. Redid the entire format, made things much more programmer friendly by adding startGame, cleanup and restart methods at the bottom of the code. Added end game screens for crashing, landing regularly, and hitting the target. All end game screens have quit buttons that work, and play again buttons that work. There is an issue when the game is restarted, it restarts but the lander falls abnormally fast. Will have to investigate later, already stayed up way later than planned. This is now the latest version of the code, copy all of what is there now. No more updates from me until later tomorrow.

--- View.java
+++ View.java
@@ -33,141 +33,8 @@ public class View extends Application {
 	
 	public void start(Stage primaryStage) {
 		
-		/* 
-		 * Main game scene and initialization.
-		 */
-		if(stage==null){
-			stage = primaryStage;
-		}
-		
-		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root,800,800);
-		scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-		root.setId("MainImage");
-		BorderPane topGUI = new BorderPane();
-		root.setTop(topGUI);
-		topGUI.setId("title");
-		MoonLander lander = new MoonLander(rocketOn,root);
-		targetLocation = generateTarget(root);
-		//Top left Vbox of the GUI that displays the stats for the rocket.
-				VBox leftStats = new VBox(12);
-				leftStats.setPrefWidth(200);
-				leftStats.setPrefHeight(150);
-				leftStats.setStyle("-fx-background-color: black;");
-				Label l1 = new Label("Fuel: ");
-				Label l2 = new Label("Velocity: ");
-				Label l3 = new Label("Thrust %: ");
-				Label l4 = new Label("Orientation: ");
-				l1.setTextFill(Color.WHITE);
-				l2.setTextFill(Color.WHITE);
-				l3.setTextFill(Color.WHITE);
-				l4.setTextFill(Color.WHITE);
-				leftStats.getChildren().addAll(l1,l2,l3,l4);
-				topGUI.setLeft(leftStats);
-				//Top right Vbox of the GUI, shows height in meters and elapsed time.
-				VBox rightStats = new VBox(12);
-				rightStats.setPrefWidth(200);
-				rightStats.setPrefHeight(150);
-				rightStats.setStyle("-fx-background-color: black");
-				Label l5 = new Label("Time Elapsed: ");
-				Label l6 = new Label("Height: ");
-				l5.setTextFill(Color.WHITE);
-				l6.setTextFill(Color.WHITE);
-				rightStats.getChildren().addAll(l5,l6);
-				topGUI.setRight(rightStats);
-				
-			scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
-				public void handle(KeyEvent e){
-					lander.processInput(e.getCode(),root);
-				
-				}
-				
-			});
-			//TimeTracker timer = new TimeTracker();
-				
-				stage.setScene(scene);
-				stage.show();
-		
-				/* 
-				 * Win, play again scene setup. Initialization located in AnimationTimer.
-				 */
-				Pane winPlayAgain = new Pane();
-				Scene scene2 = new Scene(winPlayAgain,400,363);
-				scene2.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-				winPlayAgain.setId("Vader");
-				Button btn1 = new Button("Play Again?");
-				Button btn2 = new Button("Quit");
-				btn1.setTranslateX(280);
-				btn1.setTranslateY(325);
-				btn2.setTranslateX(25);
-				btn2.setTranslateY(325); 
-				winPlayAgain.getChildren().addAll(btn1,btn2);
-				
-		
-		
-		
-		
-		
-		/* 
-		 * Game animation loop initialization.
-		 */
-		AnimationTimer gameLoop = new AnimationTimer() {
-
-            @Override
-            public void handle(long now) {
-            	
-            	l1.setText("Fuel: "+ lander.getFuel());
-            	l2.setText("Velocity: "+ lander.getVelocity());
-            	l3.setText("Thrust %: "+ lander.getThrust());
-            	l4.setText("Direction Angle: "+ lander.getDirectionAngle());
-            	//l5.setText("Time Elapsed: "+timer.getTime());
-            	l6.setText("Height: " + lander.getHeight());
-            	
-            	lander.updateLocation(lander.getX(), lander.getY(), lander.getDirectionAngle());
-            	lander.move();
-            	lander.thrust(lander.getThrust());
-            	
-            	if(lander.getY()>650){ //Hits the Ground
-            		
-            		if(lander.getDirectionAngle() < 20 || lander.getDirectionAngle() > 345){//Check to see if it Crashed (check Velocity and angles)
-            			System.out.println(targetLocation);
-            			if((targetLocation - 75) < lander.getX() && lander.getX() < (targetLocation+75)){ //Is it on the target
-            				
-                    			System.out.println("Hit the Target!");
-                    			primaryStage.setScene(scene2);
-                    			primaryStage.show();
-                    		}
-            			System.out.println("You landed, ya piece of shit.");
-            		}
-            		else{
-            			System.out.println("You Crashed, ya piece of shit.");
-            		}
-            		
-            		stop();
-            	}
-            	
-
-            }
-
-        };
-       gameLoop.start();
-       btn1.setOnAction((event) -> {
-    	    System.out.println("Play again.");
-    	    lander.reset();
-    	    removeTarget(root,image);
-    	    targetLocation = generateTarget(root);
-    	    stage.setScene(scene);
-       	    stage.show();
-       	    gameLoop.start();
-    	});
-       btn2.setOnAction((event) -> {
-   	    System.out.println("Quit");
-
-   	});
-       
-       
-       
-	}
+       startGame(primaryStage);
+       }
 	public static void main(String[] args){
 		launch(args); //move?
 	}
@@ -180,10 +47,190 @@ public class View extends Application {
 		layer.getChildren().add(image);
 		return x;
 	}
-	private void removeTarget(Pane layer, Node target){
+void removeTarget(Pane layer, Node target){
 		layer.getChildren().remove(target);
 	}
+	void cleanup(AnimationTimer a, MoonLander lander){
+		lander.reset();
+		a.stop();
+		
+	}
+void restart(Stage stage,AnimationTimer a,MoonLander lander){
+	cleanup(a,lander);
+	startGame(stage);
+}
+void startGame(Stage stage){
 
+	/* 
+	 * Main game scene and initialization.
+	 */
+	
+	BorderPane root = new BorderPane();
+	Scene scene = new Scene(root,800,800);
+	scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+	root.setId("MainImage");
+	BorderPane topGUI = new BorderPane();
+	root.setTop(topGUI);
+	topGUI.setId("title");
+	MoonLander lander = new MoonLander(rocketOn,root);
+	targetLocation = generateTarget(root);
+	//Top left Vbox of the GUI that displays the stats for the rocket.
+			VBox leftStats = new VBox(12);
+			leftStats.setPrefWidth(200);
+			leftStats.setPrefHeight(150);
+			leftStats.setStyle("-fx-background-color: black;");
+			Label l1 = new Label("Fuel: ");
+			Label l2 = new Label("Velocity: ");
+			Label l3 = new Label("Thrust %: ");
+			Label l4 = new Label("Orientation: ");
+			l1.setTextFill(Color.WHITE);
+			l2.setTextFill(Color.WHITE);
+			l3.setTextFill(Color.WHITE);
+			l4.setTextFill(Color.WHITE);
+			leftStats.getChildren().addAll(l1,l2,l3,l4);
+			topGUI.setLeft(leftStats);
+			//Top right Vbox of the GUI, shows height in meters and elapsed time.
+			VBox rightStats = new VBox(12);
+			rightStats.setPrefWidth(200);
+			rightStats.setPrefHeight(150);
+			rightStats.setStyle("-fx-background-color: black");
+			Label l5 = new Label("Time Elapsed: ");
+			Label l6 = new Label("Height: ");
+			l5.setTextFill(Color.WHITE);
+			l6.setTextFill(Color.WHITE);
+			rightStats.getChildren().addAll(l5,l6);
+			topGUI.setRight(rightStats);
+			
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+			public void handle(KeyEvent e){
+				lander.processInput(e.getCode(),root);
+			
+			}
+			
+		});
+		//TimeTracker timer = new TimeTracker();
+			
+			stage.setScene(scene);
+			stage.show();
+	
+			/* 
+			 * Win, play again or quit scene setup. Initialization located in AnimationTimer.
+			 */
+			Pane winPlayAgain = new Pane();
+			Scene scene2 = new Scene(winPlayAgain,400,363);
+			scene2.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			winPlayAgain.setId("Vader");
+			Button btn1 = new Button("Play Again?");
+			Button btn2 = new Button("Quit");
+			btn1.setTranslateX(280);
+			btn1.setTranslateY(325);
+			btn2.setTranslateX(25);
+			btn2.setTranslateY(325); 
+			winPlayAgain.getChildren().addAll(btn1,btn2);
+			
+			/* 
+			 * Land, play again or quit scene setup. Initialization located in AnimationTimer.
+			 */
+			Pane landPlayAgain = new Pane();
+			Scene scene3 = new Scene(landPlayAgain,500,206);
+			scene3.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			landPlayAgain.setId("Finn");
+			Button btn3 = new Button("Play Again?");
+			Button btn4 = new Button("Quit");
+			btn3.setTranslateX(400);
+			btn3.setTranslateY(175);
+			btn4.setTranslateX(15);
+			btn4.setTranslateY(175); 
+			landPlayAgain.getChildren().addAll(btn3,btn4);
+			/* 
+			 * Crash, play again or quit scene setup. Initialization located in AnimationTimer.
+			 */
+			Pane crashPlayAgain = new Pane();
+			Scene scene4 = new Scene(crashPlayAgain,360,240);
+			scene4.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+			crashPlayAgain.setId("Luke");
+			Button btn5 = new Button("Play Again?");
+			Button btn6 = new Button("Quit");
+			btn5.setTranslateX(270);
+			btn5.setTranslateY(210);
+			btn6.setTranslateX(0);
+			btn6.setTranslateY(210); 
+			crashPlayAgain.getChildren().addAll(btn5,btn6);
+	
+	
+	
+	/* 
+	 * Game animation loop initialization.
+	 */
+	AnimationTimer gameLoop = new AnimationTimer() {
+
+        @Override
+        public void handle(long now) {
+        	
+        	l1.setText("Fuel: "+ lander.getFuel());
+        	l2.setText("Velocity: "+ lander.getVelocity());
+        	l3.setText("Thrust %: "+ lander.getThrust());
+        	l4.setText("Direction Angle: "+ lander.getDirectionAngle());
+        	//l5.setText("Time Elapsed: "+timer.getTime());
+        	l6.setText("Height: " + lander.getHeight());
+        	
+        	lander.updateLocation(lander.getX(), lander.getY(), lander.getDirectionAngle());
+        	lander.move();
+        	lander.thrust(lander.getThrust());
+        	
+        	if(lander.getY()>650){ //Hits the Ground
+        		
+        		if(lander.getDirectionAngle() < 20 || lander.getDirectionAngle() > 345){//Check to see if it Crashed (check Velocity and angles)
+        			System.out.println(targetLocation);
+        			if((targetLocation - 75) < lander.getX() && lander.getX() < (targetLocation+75)){ //Is it on the target
+        				
+                			System.out.println("Hit the Target!");
+                			stage.setScene(scene2);
+                			stage.show();
+                		}
+        			System.out.println("You landed.");
+        			stage.setScene(scene3);
+        			stage.show();
+        		}
+        		else{
+        			System.out.println("You Crashed.");
+        			stage.setScene(scene4);
+        			stage.show();
+        		}
+        		
+        		stop();
+        	}
+        	
+
+        }
+
+    };
+   gameLoop.start();
+   btn1.setOnAction((event) -> {
+	   restart(stage, gameLoop, lander);
+	});
+   btn2.setOnAction((event) -> {
+	    System.out.println("Quit");
+	    stage.close();
+
+	});
+   btn3.setOnAction((event) -> {
+	   restart(stage, gameLoop, lander);
+	});
+   btn4.setOnAction((event) -> {
+	    System.out.println("Quit");
+	    stage.close();
+
+	});
+   btn5.setOnAction((event) -> {
+	   restart(stage, gameLoop, lander);
+	});
+   btn6.setOnAction((event) -> {
+	    System.out.println("Quit");
+	    stage.close();
+
+	});
+}
 	
 }
 
